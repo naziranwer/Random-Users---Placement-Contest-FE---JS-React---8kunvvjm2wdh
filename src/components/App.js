72aeb2a467fef2695056b9576601446b7de8ee11
naziranwer <122814116+naziranwer@users.noreply.github.com>
2023-09-03 18:31:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,32 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Users from "./users";
 
+
 const App = () => {
     const [items, setItems] = useState([]);
-    const [isLoading, setLoading] = useState(true);
+    const [isLoading, setLoading] = useState(false);
+    console.clear();
 
-    useEffect(() => {}, []);
+    function dataExtracting(data) {
+        let extracted = data.map((item)=> {
+            return item;
+        })
+        setItems(extracted);
+    }
+    
+    useEffect(() => {
+        let url = "https://randomuser.me/api/?results=10";
+        
+        setTimeout(()=> {
+            setLoading(true);
+        }, 3000)
+
+        fetch(url).then(data=>data.json()).then(data=>{            
+            dataExtracting(data.results);
+        })
+
+
+    }, []);
 
     return <Users isLoading={isLoading} items={items} />;
 };
